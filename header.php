16b3c4c74f70407f4b36429d419f95dd6e173b03
Aaron Graham <aaron.graham@wustl.edu>
2021-08-25 14:48:37 -0500

    Revert previous commit

--- header.php
+++ header.php
@@ -21,7 +21,7 @@
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 
 	<?php wp_head(); ?>
-	<script data-ad-client="ca-pub-1827813988014570" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1827813988014570" crossorigin="anonymous"></script>
 </head>
 
 <body <?php body_class(); ?>>
