5a5a41a1cddf0cc506a5b54e1f80b011d6d60fa8
Aaron Graham <aegraham@gmail.com>
2022-09-14 13:43:10 -0500

    Enqueue dashicons and make the search box a little prettier

--- functions.php
+++ functions.php
@@ -8,7 +8,7 @@ function hooniverse_scripts() {
 	$this_version = $this_theme->get( 'Version' );
 
 	// Base hooniverse style + script.
-	wp_enqueue_style( 'hooniverse-style', get_stylesheet_uri(), array(), $this_version );
+	wp_enqueue_style( 'hooniverse-style', get_stylesheet_uri(), array( 'dashicons' ), $this_version );
 	wp_enqueue_style( 'hooniverse-fonts', 'https://use.typekit.net/upb4tol.css', array(), $this_version );
 	wp_enqueue_script( 'hooniverse-js', get_stylesheet_directory_uri() . '/script.js', array(), $this_version, true );
 }
--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 window.onload = () => {
-	const searchRow = document.getElementsByClassName('search-form')[0];
-	document.getElementsByClassName('dashicons-search')[0].onclick = ( (e) => {
-		searchRow.classList.toggle('search-hidden');
+	const searchForm = document.getElementsByClassName('wp-block-search')[0];
+	document.getElementsByClassName('dashicons wp-block-navigation-link')[0].onclick = ( (e) => {
+		searchForm.classList.toggle('search-hidden')
+		e.target.classList.toggle('dashicons-no');
+		e.target.classList.toggle('dashicons-search');
 	});
 }
